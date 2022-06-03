036bc5799a7fb7a6b6475079c873b60efca97755
torpman <torpman1112@gmail.com>
2022-06-03 01:24:03 +0000

    dynamuc script debugged

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    total = len(sys.argv)
-    if total <= 1:
-        print("0 arguments.")
-    else:
-        if total == 2:
-            print("{:d} argument:".format(total - 1))
-        else:
-            print("{:d} arguments:".format(total - 1))
-        for i in range(1, total):
-            print("{:d}: {}".format(i, sys.argv[i]))
+    total = len(sys.argv) - 1
+    if total == 0:
+        print("{:d} arguments.".format(total))
+        exit()
+
+    if total == 1:
+        print("{:d} argument:".format(total))
+        for x in sys.argv[1:]:
+	    print("{}: {}".format(total, x))
+    if total > 1:
+        print("{} arguments:".format(total))
+        for x, n in enumerate(sys.argv[1:], 1):
+            print("{}: {}".format(x, n))
