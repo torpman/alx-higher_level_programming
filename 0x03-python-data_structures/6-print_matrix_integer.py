53807051aad795614ba513c655690b8e9c3cc3b9
torpman <torpman1112@gmail.com>
2022-06-06 03:00:47 +0000

    out error fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    row = len(matrix)
-    col = len(matrix[0])
-    for i in range(row):
-        for j in range(col):
-            print("{:d} ".format(matrix[i][j]), end = "")
+    for x in matrix:
+        for y in x:
+            print('{:d}'.format(y), end='')
+            if y != x[-1]:
+                print(end=' ')
         print()
